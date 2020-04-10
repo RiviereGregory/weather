0556356b1676728cc5c91a7241c7dd4ea47b8b77
RIVIERE <gregory.riviere@laposte.net>
2020-04-10 15:49:10 +0200

    Ajout list courte de pays en cas de OutOfMemoryError

--- app/src/main/java/gri/riverjach/weather/utils/JsonUtils.kt
+++ app/src/main/java/gri/riverjach/weather/utils/JsonUtils.kt
@@ -21,28 +21,54 @@ class JsonUtils {
         fun readJson(resources: Resources): ArrayList<CityName> {
 
             var citiesNames = arrayListOf<CityName>()
-            var json: String? = null
 
             try {
-                val inputStream: InputStream = resources.openRawResource(R.raw.city_list_fr)
-                json = inputStream.bufferedReader().use { it.readText() }
-
-                var jsonarr = JSONArray(json)
-
-                for (i in 0..jsonarr.length() - 1) {
-                    var jsonobj = jsonarr.getJSONObject(i)
-                    citiesNames.add(
-                        CityName(
-                            jsonobj.getString("name"),
-                            jsonobj.getString("country")
-                        )
-                    )
-                }
-
+                parseJson(resources, R.raw.city_list, citiesNames)
+            } catch (out: OutOfMemoryError) {
+                citiesNames.clear()
+                shortListOfCountries(resources, citiesNames)
+                Log.i("JsonUtils", "OutMemory load less pays")
             } catch (e: Exception) {
                 Log.e("JsonUtils", "Erreur parsing", e)
             }
             return citiesNames
+        }
+
+        private fun shortListOfCountries(
+            resources: Resources,
+            citiesNames: ArrayList<CityName>
+        ) {
+            parseJson(resources, ListCountries.FR.pays, citiesNames)
+            parseJson(resources, ListCountries.IT.pays, citiesNames)
+            parseJson(resources, ListCountries.ES.pays, citiesNames)
+            parseJson(resources, ListCountries.US.pays, citiesNames)
+            parseJson(resources, ListCountries.GB.pays, citiesNames)
+            parseJson(resources, ListCountries.CA.pays, citiesNames)
+            parseJson(resources, ListCountries.DE.pays, citiesNames)
+            parseJson(resources, ListCountries.BE.pays, citiesNames)
+            parseJson(resources, ListCountries.CH.pays, citiesNames)
+        }
+
+        private fun parseJson(
+            resources: Resources,
+            pays: Int,
+            citiesNames: ArrayList<CityName>
+        ) {
+            var json: String? = null
+            val inputStream: InputStream = resources.openRawResource(pays)
+            json = inputStream.bufferedReader().use { it.readText() }
+
+            var jsonarr = JSONArray(json)
+
+            for (i in 0..jsonarr.length() - 1) {
+                var jsonobj = jsonarr.getJSONObject(i)
+                citiesNames.add(
+                    CityName(
+                        jsonobj.getString("name"),
+                        jsonobj.getString("country")
+                    )
+                )
+            }
         }
     }
 }
